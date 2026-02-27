cc1d4a259e861e247e7ffe3749ead950dd3c7ed9
serbanraiescu <serbanraiescu@gmail.com>
2026-02-27 12:23:59 +0200

    f

--- index-root.php
+++ index-root.php
@@ -6,19 +6,19 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // --- Daser Root Proxy ---
-// This file is used for cPanel deployments where index.php is in the root.
+$APP_DIR = __DIR__ . '/daser_gym_app';
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $APP_DIR.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+require $APP_DIR.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once $APP_DIR.'/bootstrap/app.php';
 
 // Force public path to public folder for root access
 $app->usePublicPath(__DIR__.'/public');
