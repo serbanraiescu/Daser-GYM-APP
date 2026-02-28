adb06969a86a86675b73b0eda1a007f386037b1d
serbanraiescu <serbanraiescu@gmail.com>
2026-02-28 20:49:28 +0200

    Enhance UI: Increase Member creation modal width to 5xl

--- app/Filament/Resources/Members/Pages/ListMembers.php
+++ app/Filament/Resources/Members/Pages/ListMembers.php
@@ -15,7 +15,7 @@ class ListMembers extends ListRecords
         return [
             CreateAction::make()
                 ->label('Adaugă membru')
-                ->modalWidth('3xl')
+                ->modalWidth('5xl')
                 ->after(function (array $data, \App\Models\Member $record) {
                     if ($data['activate_plan'] ?? false) {
                         $plan = \App\Models\Plan::find($data['initial_plan_id']);
